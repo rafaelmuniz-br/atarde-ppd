61b204ad2368cac73385c86a5208628616725165
Rafael <rafael.s.m.muniz@gmail.com>
2025-08-29 13:09:44 -0300

    Index Custom-Btn

--- components/index/TextImageSection-PremioEco.vue
+++ components/index/TextImageSection-PremioEco.vue
@@ -5,7 +5,7 @@
       description="O Prêmio EcoInovar é voltado para professores e estudantes da rede pública estadual de ensino, incentivando o desenvolvimento e a implementação de projetos científicos inovadores e sustentáveis.  A premiação abrange escolas e comunidades conectando educação, ciência e sustentabilidade para transformar realidades locais."
       imageSrc="/premio-eco.png"
       imageAlt="Sobre o EcoInovar"
-      buttonText="Inscrições abertas"
+      buttonText="Inscrições abertas →"
       buttonLink="/"
       :reverse="true"
     />
@@ -15,3 +15,17 @@
     import TextImageSection from '@/components/TextImageSection.vue'
   </script>
     ~/components/TextImageSection copy.vue
+
+  
+<style scoped>
+::v-deep(.btn-primary){
+      display: inline-block;
+      background: none;
+      color: #F08A3C;
+      padding: 0px 0px;
+      border-radius: 0px;
+      text-decoration: none;
+      font-family: 'Codec Warm Trial', sans-serif;
+      font-weight: 700;
+    }
+</style>
